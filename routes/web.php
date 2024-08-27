9934f0c5559fdf47ea63d3bfbffd77dfa6afdfca
Josue Diaz <jdiaz77711@gmail.com>
2024-08-27 10:15:15 +0100

    Feature: Add final setup

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,4 @@ Route::post('/flights', [FlightController::class, 'store'])->name('flights.store
 Route::delete('/flights', [FlightController::class, 'destroy'])->name('flights.destroy');
 Route::patch('/flights', [FlightController::class, 'update'])->name('flights.update');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__ . '/auth.php';
